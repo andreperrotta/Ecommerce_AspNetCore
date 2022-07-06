278df7e9b73080db26220eb6cbd850f734fcab78
andreperrotta <andre.perrotta@hotmail.com>
2022-07-05 21:10:49 -0300

    Implementando Entidade de Endereço

---
+++ Ecommerce/Src/Business/Models/Endereco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class Endereco
+    {
+        // prop tab tab
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        // Clrt D copia
+    }
+}
