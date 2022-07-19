433101f10712da18445da76f4109f3662309425e
andreperrotta <andre.perrotta@hotmail.com>
2022-07-19 20:31:05 -0300

    Criando Contexto de Banco de Dados

---
+++ Ecommerce/Src/Data/Context/MeuDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context
+{
+    class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
